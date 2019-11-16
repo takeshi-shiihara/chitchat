e3b1cddaa1de4d2de9fdea10ea3ce073232095d4
takeshi-shiihara <shi4490@icloud.com>
2019-11-16 17:56:08 +0900

    Initial commit

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	files := http.FileServer(http.Dir("/public"))
+	mux.Handle("/static/", http.StripPrefix("/static/", files))
+
+	//index
+	mux.HandleFunc("/", index)
+	//error
+	mux.HandleFunc("/err", err)
+
+	//defind in route_auth.go
+	mux.HandleFunc("/login", login)
+	mux.HandleFunc("/logout", logout)
+	mux.HandleFunc("/signup", signup)
+	mux.HandleFunc("/signup_account", signupAccount)
+	mux.HandleFunc("/authenticate", authenticate)
+
+	//defined in route_thread .go
+	mux.HandleFunc("/thread/new", newThread)
+	mux.HandleFunc("/thread/create", createThread)
+	mux.HandleFunc("/thread/post", postThread)
+	mux.HandleFunc("/thread/read", readThread)
+
+	//starting up the server
+	server := &http.Server{
+		Addr:    "0.0.0.0.8080",
+		Handler: mux,
+	}
+	server.ListenAndServe()
+}
