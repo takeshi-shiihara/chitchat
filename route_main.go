c35f075368ddc5357a5af96619f3e3cdb238b51f
takeshi-shiihara <shi4490@icloud.com>
2019-11-18 15:17:40 +0900

    index

---
+++ route_main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"net/http"
+	"text/template"
+)
+
+func index(w http.ResponseWriter, r *http.Request) {
+	threads, err := data.Threads()
+	if err == nil {
+		_, err := session(w, r)
+		public_tmpl_files := []string{"templates/layout.html",
+			"templates/public.nevbar.html",
+			"templates/index.html"}
+		private_tmpl_files := []string{"templates/layout.html",
+			"templates/private.navbar.html",
+			"templates/index.html"}
+		var templates *template.Template
+		if err != nil {
+			templates = template.Must(template.ParseFiles(private_tmpl_files))
+		} else {
+			templates = template.Must(template.ParseFiles(public_tmpl_files))
+		}
+		templates.ExecuteTemplate(w, "layout", threads)
+	}
+}
